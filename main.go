cfae8e6866f1b9c743b05f751b448a724e26bc8e
M4n5ter <contact@m4n5ter.email>
2023-08-16 18:00:43 +0800

    feat: 优先获取ipv4公网地址

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"regexp"
 	"sync"
+	"time"
 
 	"golang.org/x/exp/slog"
 
@@ -51,25 +53,72 @@ func main() {
 // 获取 public ip TODO: 可以提供更多的 public ip 获取方式，并且可以在其中一个获取失败时切换到另一个
 func getPublicIp() net.Addr {
 	Logger.Info("正在获取公网IP,请稍后...", "check_ip_url", Config.CheckIpUrl)
-	response, err := http.Get(Config.CheckIpUrl)
-	defer response.Body.Close()
+	conn := connectUrl(Config.CheckIpUrl)
+	defer conn.Close()
+
+	// 组装一个使用自定义连接的 http client
+	transport := &http.Transport{
+		Dial: func(network, addr string) (net.Conn, error) {
+			return conn, nil
+		},
+	}
+	client := &http.Client{
+		Timeout:   time.Second * 15,
+		Transport: transport,
+	}
+
+	response, err := client.Get(Config.CheckIpUrl)
 	if err != nil {
 		log.LogPanic(Logger, fmt.Sprintf("请求 %s 失败: %v\n", Config.CheckIpUrl, err))
 	}
+	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.LogPanic(Logger, fmt.Sprintf("获取公网IP失败: %s\n", err))
 	}
 
-	// use regex to get ip
 	re := regexp.MustCompile(`(?m)(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
 	ipStr := re.FindString(string(body))
-	ip, err := net.ResolveIPAddr("ip", ipStr)
+	pip, err := net.ResolveIPAddr("ip", ipStr)
 	if err != nil {
 
 		log.LogPanic(Logger, fmt.Sprintf("解析公网IP失败: %s\n", err))
 	}
-	Logger.Info("公网IP获取成功:", "ip", ip)
-	return ip
+	Logger.Info("公网IP获取成功:", "ip", pip)
+	return pip
+}
+
+// connectUrl 生成一个优先使用 IPv4 的连接
+func connectUrl(uri string) net.Conn {
+	parsedUrl, err := url.Parse(uri)
+	if err != nil {
+		log.LogPanic(Logger, fmt.Sprintf("解析 %s 失败: %v\n", uri, err))
+	}
+
+	host := parsedUrl.Hostname()
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.LogPanic(Logger, fmt.Sprintf("Lookup %s 失败: %v\n", host, err))
+	}
+
+	var ip net.IP
+	// v4
+	for _, i := range ips {
+		if i.To4() != nil {
+			ip = i
+			break
+		}
+	}
+	// v6
+	if ip == nil {
+		ip = ips[0].To16()
+	}
+
+	conn, err := net.Dial("tcp", ip.String()+":443")
+	if err != nil {
+		log.LogPanic(Logger, fmt.Sprintf("连接 %s 失败: %v\n", Config.CheckIpUrl, err))
+	}
+
+	return conn
 }
